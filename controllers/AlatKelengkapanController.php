6fce02b92f0fb929e8a44328efabcc4d59b93928
unknown <piant.grunger@gmail.com>
2019-01-02 14:00:24 +0700

    update

--- controllers/AlatKelengkapanController.php
+++ controllers/AlatKelengkapanController.php
@@ -9,8 +9,6 @@ use app\models\AlatKelengkapanSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
-use app\models\PersonilSearch;
-use yii\data\Pagination;
 use yii\helpers\Json;
 use app\models\Personil;
 
@@ -20,7 +18,7 @@ use app\models\Personil;
 class AlatKelengkapanController extends Controller
 {
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function behaviors()
     {
@@ -36,6 +34,7 @@ class AlatKelengkapanController extends Controller
 
     /**
      * Lists all AlatKelengkapan models.
+     *
      * @return mixed
      */
     public function actionIndex()
@@ -48,23 +47,46 @@ class AlatKelengkapanController extends Controller
             'dataProvider' => $dataProvider,
         ]);
     }
+
     public function actionIndexMapping()
     {
         $searchModel = new AlatKelengkapanSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         if ($searchModel->tahun == '') {
-            $searchModel->tahun=date('Y');
+            $searchModel->tahun = date('Y');
         }
+
         return $this->render('index-mapping', [
             'searchModel' => $searchModel,
             'dataProvider' => $dataProvider,
         ]);
     }
 
+    public function actionCopy()
+    {
+        $alatKelengkapan = AlatKelengkapan::find()->where(['tahun' => date('Y') - 1])->all();
+        foreach ($alatKelengkapan as $data) {
+            $new = new AlatKelengkapan();
+            $new->alat_kelengkapan = $data->alat_kelengkapan;
+            $new->tahun = $data->tahun + 1;
+            $new->save();
+            foreach ($data->detailAlatKelengkapan as $detail) {
+                $newDetail = new DetAlatKelengkapan();
+                $newDetail->id_alat_kelengkapan = $new->alat_kelengkapan;
+                $newDetail->id_personil = $detail->id_personil;
+                $newDetail->jenis = $detail->jenis;
+                $newDetail->save();
+            }
+        }
+        $this->redirect('index');
+    }
+
     /**
      * Displays a single AlatKelengkapan model.
-     * @param integer $id
+     *
+     * @param int $id
+     *
      * @return mixed
      */
     public function actionView($id)
@@ -77,6 +99,7 @@ class AlatKelengkapanController extends Controller
     /**
      * Creates a new AlatKelengkapan model.
      * If creation is successful, the browser will be redirected to the 'view' page.
+     *
      * @return mixed
      */
     public function actionCreate()
@@ -95,7 +118,9 @@ class AlatKelengkapanController extends Controller
     /**
      * Updates an existing AlatKelengkapan model.
      * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
+     *
+     * @param int $id
+     *
      * @return mixed
      */
     public function actionUpdate($id)
@@ -111,14 +136,13 @@ class AlatKelengkapanController extends Controller
         }
     }
 
-
     public function actionMapping($id)
     {
         $model = $this->findModel($id);
-        if (count($model->detailAlatKelengkapan) ===0) {
+        if (count($model->detailAlatKelengkapan) === 0) {
             $list = [];
             $detail = new DetAlatKelengkapan();
-            $detail->id_personil=102;
+            $detail->id_personil = 102;
             $detail->id_alat_kelengkapan = $model->id_alat_kelengkapan;
             $detail->jenis = 'Ketua DPRD';
 
@@ -147,7 +171,6 @@ class AlatKelengkapanController extends Controller
             */
         }
 
-
         if ($model->load(Yii::$app->request->post())) {
             $transaction = Yii::$app->db->beginTransaction();
             try {
@@ -155,6 +178,7 @@ class AlatKelengkapanController extends Controller
                 //die(var_dump($model->detailAlatKelengkapan));
                 if ($model->save()) {
                     $transaction->commit();
+
                     return $this->redirect(['index-mapping']);
                 }
             } catch (\yii\db\IntegrityException $e) {
@@ -167,14 +191,16 @@ class AlatKelengkapanController extends Controller
         } else {
             return $this->render('mapping', [
                 'model' => $model,
-
         ]);
         }
     }
+
     /**
      * Deletes an existing AlatKelengkapan model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
+     *
+     * @param int $id
+     *
      * @return mixed
      */
     public function actionDelete($id)
@@ -182,29 +208,32 @@ class AlatKelengkapanController extends Controller
         try {
             $this->findModel($id)->delete();
         } catch (\yii\db\IntegrityException  $e) {
-            Yii::$app->session->setFlash('error', "Data Tidak Dapat Dihapus Karena Dipakai Modul Lain");
+            Yii::$app->session->setFlash('error', 'Data Tidak Dapat Dihapus Karena Dipakai Modul Lain');
         }
+
         return $this->redirect(['index']);
     }
 
     public function actionPersonil($id)
     {
         $model = Personil::findOne(['id_personil' => $id]);
+
         return Json::encode([
             'id_personil' => $model->id_personil,
             'nama_personil' => $model->nama_personil,
             'status_personil' => $model->status_personil,
             'nama_pangkat' => $model->nama_pangkat,
-
-
         ]);
     }
 
     /**
      * Finds the AlatKelengkapan model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
+     *
+     * @param int $id
+     *
      * @return AlatKelengkapan the loaded model
+     *
      * @throws NotFoundHttpException if the model cannot be found
      */
     protected function findModel($id)
--- views/alat-kelengkapan/index.php
+++ views/alat-kelengkapan/index.php
@@ -4,14 +4,14 @@
 use hscstudio\mimin\components\Mimin;
 use yii\helpers\Html;
 use kartik\grid\GridView;
- use kartik\export\ExportMenu;
-$gridColumns=[['class' => 'kartik\grid\SerialColumn'], 
+
+$gridColumns = [['class' => 'kartik\grid\SerialColumn'],
             'alat_kelengkapan',
             'tahun',
 
          ['class' => 'kartik\grid\ActionColumn',   'template' => Mimin::filterActionColumn([
-              'update','delete','view'],$this->context->route),    ],    ];
-
+              'update', 'delete', 'view', ], $this->context->route),
+            ],    ];
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\AlatKelengkapanSearch */
@@ -22,7 +22,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="alat-kelengkapan-index">
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]);?>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
@@ -38,11 +38,13 @@ $this->params['breadcrumbs'][] = $this->title;
 
         'panel' => [
             'type' => GridView::TYPE_INFO,
-             'heading' => '<i class="glyphicon glyphicon-tasks"></i>  <strong> '.Yii::t('app', 'Alat Kelengkapan'). '</strong>',
+             'heading' => '<i class="glyphicon glyphicon-tasks"></i>  <strong> '.Yii::t('app', 'Alat Kelengkapan').'</strong>',
       ],
             'toolbar' => [
-        ['content' => ((Mimin::checkRoute($this->context->id . "/create"))) ?         Html::a(Yii::t('app', 'Alat Kelengkapan Baru'), ['create'], ['class' => 'btn btn-success']) :""],
-
+        ['content' => ((Mimin::checkRoute($this->context->id.'/create'))) ? Html::a(Yii::t('app', 'Alat Kelengkapan Baru'), ['create'], ['class' => 'btn btn-success']) : '',
+    ],
+    ['content' => ((Mimin::checkRoute($this->context->id.'/copy'))) ? Html::a(Yii::t('app', 'Copy Alat Kelengkapan Tahun Lalu'), ['copy'], ['class' => 'btn btn-info']) : '',
+],
         '{export}',
         '{toggleData}',
     ],
